c63ae7b671a8c880eda379ef26e1821270a87100
RamiroMendoza16 <rms16121305@outlook.com>
2020-04-01 18:38:39 -0600

    Version 1.0.0

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.carousel').carousel();
+    $('.carousel.carousel-slider').carousel({
+    });
+    $('.materialboxed').materialbox();
+    $('.modal').modal();
+    $('.sidenav').sidenav();
+    $('select').formSelect();
+    $('.datepicker').datepicker();
+    $('.timepicker').timepicker();
+    $('.parallax').parallax();
+
+
+
+  });
